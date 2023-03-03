7c47f574f01ccbaea74ae5a461c2f0c5ad95d624
Jagan0901 <Jaganath0901@gmail.com>
2023-03-03 13:40:21 +0530

    added token

--- src/Add_Movies.js
+++ src/Add_Movies.js
@@ -216,7 +216,7 @@ export function Add_Movies() {
           fetch(`${movieAPI}/Movies`,{
             method :"POST",
             body   :JSON.stringify(newMovie),
-            headers: {"Content-Type" : "application/json"}
+            headers: {"Content-Type" : "application/json", "x-auth-token" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZGJkZWYzNDYwNzhlMzVlZjM1ZDg3ZiIsImlhdCI6MTY3NTM1NTAyOX0.yavdcq05w3wK_6HYXxMq9KtJ6qVZ12E3GxuEXt_eOSo"}
             })
             .then((data)=>data.json())
             .then(()=> navigate("/Movies"))
