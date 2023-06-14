b0a6bd7e51b858f56bfbab2235c711f7db23d7c0
Jagan0901 <Jaganath0901@gmail.com>
2023-06-14 10:37:50 +0530

    init commit

--- src/Home.js
+++ src/Home.js
@@ -11,7 +11,7 @@ export function Home() {
   };
   return (
     <div style={titleStyles}>
-      <h2 style={styles}>Welcome to Mini OTT Streaming app</h2>
+      <h2 style={styles}>Welcome to Shows Reviewing app</h2>
       <img src="https://cdn.iconscout.com/icon/free/png-128/netflix-5336121-4497702.png" alt="Netflix" />
     </div>
   );
